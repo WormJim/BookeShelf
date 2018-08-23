7118a906f11cc5f06756f78a535e8fdf1130079d
Grahn Cooledge <gcooled@yahoo.com>
2018-08-22 23:28:59 -0400

    added mongoose package & updated gitignore

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
+// DB Connection
+const db = require('./api/config/db').database;
+// mongoose.connect(db);
+// mongoose.connect.on('connected', () => {
+//   console.log('Databse connected: ' + db);
+// });
+// mongoose.connect.on('error', err => {
+//   console.log('Databse error: ' + db);
+// });
 
-mongoose.connect.on('connected', err => {
-  console.log('Databse error: ' + config.database);
-
-  mongoose.connect.on('error', err => {
-  console.log('Databse error: ' + config.database);
-})
+console.log(db);
--- config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  database: 'mongodb://GrahnC:<C!@10/g28N>@ds229312.mlab.com:29312/bookshelf'
-};
